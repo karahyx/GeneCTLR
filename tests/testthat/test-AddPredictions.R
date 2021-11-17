be4901dce6ec13637001fb6365d8db918b590a43
Kara Han <kara.han@mail.utoronto.ca>
2021-11-17 16:54:34 -0500

    updated the name for the addPredictions function in test-AddPredictions.R

--- tests/testthat/test-AddPredictions.R
+++ tests/testthat/test-AddPredictions.R
@@ -6,7 +6,7 @@ test_that("a data frame with one dependent variable and multiple independent
             dat <- twoClassSim(200)
             length(dat) # 16
             results <- trainCV(data = dat, col_index = 16)
-            new_dat <- AddPredictions(results, dat)
+            new_dat <- addPredictions(results, dat)
 
             expect_type(results, "list")
             expect_s3_class(new_dat, "data.frame")
@@ -19,7 +19,7 @@ test_that("a data frame with one dependent variable and one independent
             dat2 <- subset(dat, select = c(1, 16))
             length(dat2) # 2
             results <- trainCV(data = dat2, col_index = 2)
-            new_dat <- AddPredictions(results, dat2)
+            new_dat <- addPredictions(results, dat2)
 
             expect_type(results, "list")
             expect_s3_class(new_dat, "data.frame")
@@ -29,9 +29,9 @@ test_that("a data frame with one dependent variable and one independent
 test_that("TrainCV error upon invalid user input", {
   # results provided as a string
   results <- "train"
-  expect_error(new_dat <- AddPredictions(results, dat))
+  expect_error(new_dat <- addPredictions(results, dat))
 
   # data provided as a list
   dat <- list(a = c(0, 293, 1), b = c(2, 0, 20))
-  expect_error(new_dat <- AddPredictions(results, dat))
+  expect_error(new_dat <- addPredictions(results, dat))
 })
